86b00085a129cc7ed4e57b96662a41afece4794f
mina <m1nna@naver.com>
2022-08-18 00:21:37 +0900

    Authorization header 값에 따른 처리

--- src/main/java/com/example/tamna/controller/AuthController.java
+++ src/main/java/com/example/tamna/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.tamna.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class AuthController {
     @GetMapping("/login")
     public void reqLogin(){
 
+    }
+
+    @PostMapping("token")
+    public String token() {
+        return "<h1>token</h1>";
     }
 }
--- src/main/java/com/example/tamna/filter/MyFilter1.java
+++ src/main/java/com/example/tamna/filter/MyFilter1.java
@@ -10,8 +10,6 @@ public class MyFilter1 implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         System.out.println("필터1");
-//        PrintWriter out = response.getWriter();
-//        out.print("안녕");  프로세스 진행 안되고 끝남
         chain.doFilter(request, response); // 프로세스를 계속 진행하기 위해선 체인에 걸어줘야 함
     }
 }
